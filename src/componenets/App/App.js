88402913199e4aa95e64e09b1221e34bf1abc411
bikhra1 <talhaparvaizchaudhary@gmail.com>
2023-10-24 11:21:33 +0500

    update

--- src/componenets/App/App.js
+++ src/componenets/App/App.js
@@ -6,14 +6,72 @@ import SearchBar from "../SearchBar/SearchBar";
 import SearchResults from "../SearchResults/SearchResults";
 import Spotify from "../Utils/Spotify";
 
+
+/*
+
+const dummyData = [
+  { id: 1, name: 'Song 1', artist: 'Artist 1' },
+  { id: 2, name: 'Song 2', artist: 'Artist 2' },
+  { id: 3, name: 'Song 3', artist: 'Artist 3' },
+];
+
+
+const dummyData = {
+  "Song 1": [
+    { id: 1, artist: "Artist 1" },
+  ],
+  "Song 2": [
+    { id: 2, artist: "Artist 2" },
+    { id: 3, artist: "Artist 3" },
+  ],
+  "Song 3": [
+    { id: 4, artist: "Artist 4" },
+  ],
+};
+*/
+
 const App = () => {
   const [searchResults, setSearchResults] = useState([]);
   const [playlistName, setPlaylistName] = useState("New Playlist");
   const [playlistTracks, setPlaylistTracks] = useState([]);
 
+/**************************************************************/
+/**************************************************************/
+/**************************************************************/
+
+/*
+  const search = useCallback((term) => {
+    Spotify.search(term).then(setSearchResults);
+  }, []);*/
+
+  /*
+  const search = useCallback((term) => {
+    // Filter the dummyData array to simulate the search
+    const results = dummyData.filter((track) =>
+      track.name.toLowerCase().includes(term.toLowerCase())
+    );
+    setSearchResults(results);
+  }, []);
+
+  const search = useCallback((term) => {
+    // Filter the songs based on the search term
+    const results = Object.keys(dummyData)
+      .filter(title => title.toLowerCase().includes(term.toLowerCase()))
+      .map(title => dummyData[title])
+      .flat();
+  
+    setSearchResults(results);
+  }, []);
+  */
   const search = useCallback((term) => {
     Spotify.search(term).then(setSearchResults);
   }, []);
+
+
+/**************************************************************/
+/**************************************************************/
+/**************************************************************/
+
 
   const addTrack = useCallback(
     (track) => {
--- src/componenets/SearchResults/SearchResults.js
+++ src/componenets/SearchResults/SearchResults.js
@@ -6,7 +6,8 @@ import TrackList from "../TrackList/TrackList";
 const SearchResults = (props) => {
    const { onAdd, searchResults } = props;
    return (
-      <div className="w-full min-h-screen p-1 bg-opacity-70 bg-blue-900 shadow-md border-solid border-2 border-neutral-950 me-4">
+      <div className="w-full min-h-screen p-1 bg-opacity-70 bg-blue-900 shadow-md border-solid
+       border-2 border-neutral-950 me-4">
          <h2>Results:</h2>
          <TrackList tracks={searchResults} onAdd={onAdd} />
       </div>
